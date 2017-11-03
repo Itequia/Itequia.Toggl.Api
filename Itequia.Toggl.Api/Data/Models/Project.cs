587cfb1adf78dbb385288f4ddef0dc773bf3e931
Susana Castela <susana.castela@itequia.com>
2017-11-03 16:16:07 +0100

    add seed

--- Itequia.Toggl.Api/Data/Models/Project.cs
+++ Itequia.Toggl.Api/Data/Models/Project.cs
@@ -9,6 +9,12 @@ namespace Itequia.Toggl.Api.Data.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Status { get; set; }
+        public Status Status { get; set; }
+    }
+
+    public enum Status
+    {
+        Inactive,
+        Active
     }
 }
--- Itequia.Toggl.Api/Migrations/20171103150955_ChangeProjectStatus.Designer.cs
+++ Itequia.Toggl.Api/Migrations/20171103150955_ChangeProjectStatus.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Itequia.Toggl.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171103150955_ChangeProjectStatus")]
+    partial class ChangeProjectStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
